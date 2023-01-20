f9015981dffec554e3fea0a38d95e9b4c6597b0d
dmitriy <dima-dim@yandex.ru>
2023-01-20 13:57:28 +0300

    Задача 41 решена

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Number()
+{
+    Console.Clear();
+    Console.WriteLine("Для остановки программы нажите 'q'.\nВведите числа больше нуля: ");
+    bool GoCode = true;
+    int result = 0;
+    while (GoCode)
+    {
+        string m = Console.ReadLine();
+        if (int.TryParse(m, out int n) && Convert.ToInt32(m) > 0)
+        {
+            result++;
+        }
+        else if (m == "q")
+        {
+            Console.Write($"Вы нажали 'q', Программа остановилась. Введено {result} значений.\n");
+            GoCode = false;
+        }
+    }
+}
+
+Number();
+
